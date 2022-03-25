c3c45da87d3db34989e15f8cb907fc92127fc173
Luis Miguel Jaime <luism.jaimeh@gmail.com>
2022-03-25 14:43:04 -0400

    .

---
+++ src/components/auth/LoginScreen.js
@@ -0,0 +1,78 @@
+import React from 'react';
+
+export const LoginScreen = () => {
+    return (
+        <div className="container login-container">
+            <div className="row">
+                <div className="col-md-6 login-form-1">
+                    <h3>Access</h3>
+                    <form>
+                        <div className="form-group">
+                            <input 
+                                type="text"
+                                className="form-control"
+                                placeholder="Email"
+                            />
+                        </div>
+                        <div className="form-group">
+                            <input
+                                type="password"
+                                className="form-control"
+                                placeholder="Password"
+                            />
+                        </div>
+                        <div className="form-group">
+                            <input 
+                                type="submit"
+                                className="btnSubmit"
+                                value="Login" 
+                            />
+                        </div>
+                    </form>
+                </div>
+
+                <div className="col-md-6 login-form-2">
+                    <h3>Register</h3>
+                    <form>
+                        <div className="form-group">
+                            <input
+                                type="text"
+                                className="form-control"
+                                placeholder="Name"
+                            />
+                        </div>
+                        <div className="form-group">
+                            <input
+                                type="email"
+                                className="form-control"
+                                placeholder="Email"
+                            />
+                        </div>
+                        <div className="form-group">
+                            <input
+                                type="password"
+                                className="form-control"
+                                placeholder="Password" 
+                            />
+                        </div>
+
+                        <div className="form-group">
+                            <input
+                                type="password"
+                                className="form-control"
+                                placeholder="Confirm your password" 
+                            />
+                        </div>
+
+                        <div className="form-group">
+                            <input 
+                                type="submit" 
+                                className="btnSubmit" 
+                                value="Create Account" />
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ src/routers/DashboardRoute.js
@@ -0,0 +1,14 @@
+import {Routes} from 'react-router-dom';
+
+export const DashboardRoutes = () => {
+    return (
+        <>
+        <div className='container'>
+        <h1>Dashboard</h1>
+            <Routes>
+                
+            </Routes>
+        </div>
+        </>
+    )
+}
