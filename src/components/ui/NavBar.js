d43d4bc6281edc72ab3f83aa1266302ba1090bb2
Luis Miguel Jaime <luism.jaimeh@gmail.com>
2022-03-26 10:39:56 -0400

    created CalendarScreen component

--- src/components/ui/NavBar.js
+++ src/components/ui/NavBar.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 
 export const NavBar = () => {
   return (
-    <></>
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+        <Container>
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            </Nav>
+            <Nav>
+            <Nav.Link href="/">More deets</Nav.Link>
+            <Nav.Link eventKey={2} href="/">
+                Dank memes
+            </Nav.Link>
+            <button className='btn btn-outline-danger'>
+                <i class="fa-solid fa-arrow-right-from-bracket"></i>
+                Logout
+            </button>
+            </Nav>
+        </Navbar.Collapse>
+        </Container>
+        </Navbar>
   )
 }
--- src/routers/DashboardRoute.js
+++ src/routers/DashboardRoute.js
@@ -1,9 +1,13 @@
 import {Routes} from 'react-router-dom';
+import { CalendarScreen } from '../components/calendar/CalendarScreen';
 
 export const DashboardRoutes = () => {
     return (
         <>
         <div className='container'>
+        
+        <CalendarScreen/>
+
         <h1>Dashboard</h1>
             <Routes>
                 
