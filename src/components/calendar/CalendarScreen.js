413e5424862d92771b11b64017f17fd6827177bc
Luis Miguel Jaime <luism.jaimeh@gmail.com>
2022-04-04 23:24:39 -0400

    added eventPropGetter to Calendar component to add Style on selected dates

--- src/components/calendar/CalendarScreen.js
+++ src/components/calendar/CalendarScreen.js
@@ -1,17 +1,41 @@
 import React from 'react';
-import { NavBar } from '../ui/NavBar';
-import { Calendar, momentLocalizer } from 'react-big-calendar';
 import moment from 'moment';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import { NavBar } from '../ui/NavBar';
+import 'moment/locale/es';
+
+// uncomment for spanish mode
+//import { messages } from '../../helpers/calendarMessagesEsp';
+
+// uncomment for spanish mode
+// moment.locale('es');
 
 const localizer = momentLocalizer(moment);
+
 const events = [{
   title: 'Birthday Chikee',
   start: moment().toDate(),
   end: moment().add(2, 'hours').toDate(),
-  gbcolor: '#fafafa'
+  gbcolor: '#fafafa',
+  notes: 'Buy a cake'
 }]
 
 export const CalendarScreen = () => {
+
+  const eventStyleGetter = (event, start, end, isSelected) => {
+
+    const style = {
+      backgroundColor: '#367CF7',
+      borderRadius: '0px',
+      opacity: 0.8,
+      display: 'block'
+    }
+
+      return {
+        style
+      }
+  };
+
   return (
     <div>
         <NavBar/>
@@ -21,7 +45,11 @@ export const CalendarScreen = () => {
           events={events}
           startAccessor="start"
           endAccessor="end"
-          style={{ height: 700 }}
+          style={{ height: 500 }}
+          eventPropGetter={eventStyleGetter}
+          // uncomment for spanish mode
+          // messages={messages}
+
         />
     </div>
   )
