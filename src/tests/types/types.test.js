99acdc9d939f2d96fe48c2d47268ac50d35e556d
Luis Miguel Jaime <luism.jaimeh@gmail.com>
2022-05-25 12:10:12 -0400

    unit tests on types file and fetch WIP

---
+++ src/tests/types/types.test.js
@@ -0,0 +1,29 @@
+import { types } from "../../types/types"
+
+describe('Tests on types file', () => {
+
+    test('should return types object', () => { 
+
+        expect(types).toEqual({
+
+            uiOpenModal: '[ui] Open Modal',
+            uiCloseModal: '[ui] Close Modal',
+
+            eventAddNew: '[event] Add new',
+            eventStartAddNew: '[event] Start add new',
+            eventSetActive: '[event] Set active',
+            eventClearActive: '[event] Clear active',
+            eventUpdated: '[event] Updated',
+            eventDeleted: '[event] Deleted',
+            eventLoaded: '[event] Load events',
+            eventLogout: '[event] clean events',
+
+            authCheckDone: '[auth] Finish check login state',
+            authStartLogin: '[auth] Start login',
+            authLogin: '[auth] Login',
+            authStartRegister: '[auth] Start register',
+            authTokenRenew: '[auth] Token renew',
+            authLogout: '[auth] logout'
+        })
+     })
+})
